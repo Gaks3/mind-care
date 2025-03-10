432828dceb8a32a5c4c4fe5241a55ddd38c4d6fd
arkhanardana <hanzmantap@gmail.com>
2025-03-10 17:32:16 +0700

    chore: change to indonesia

--- src/lib/auth-schema.ts
+++ src/lib/auth-schema.ts
@@ -1,20 +1,16 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const formSchema = z.object({
   name: z
     .string()
-    .min(2, { message: 'Name at least 2 characters long' })
-    .max(50, { message: 'Name cant more than 50 characters long' }),
-  email: z
-    .string()
-    .email({ message: 'Please enter a valid email address' })
-    .min(2)
-    .max(50),
+    .min(2, { message: "Nama harus diisi dengan minimal 2 karakter" })
+    .max(50, { message: "Nama tidak boleh lebih dari 50 karakter" }),
+  email: z.string().email({ message: "Silahkan masukkan email yang valid" }).min(2).max(50),
 
   password: z
     .string()
-    .min(8, { message: 'Password must at least 8 characters long' })
-    .max(50, { message: 'Password cant more than 50 character long' }),
+    .min(8, { message: "Password harus diisi dengan minimal 8 karakter" })
+    .max(50, { message: "Password tidak boleh lebih dari 50 karakter" }),
 });
 
 export const signInSchema = formSchema.pick({
