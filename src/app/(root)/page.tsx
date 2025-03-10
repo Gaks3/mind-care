3a985cdf4daa57c6592187290ef7839c7dc32246
ArifRTzy <arifr0751@gmail.com>
2025-03-11 02:00:54 +0700

    add the components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,15 @@
 import About from "@/components/about";
 import Hero from "@/components/hero";
+import Topik from "@/components/topik";
+import Popularity from "@/components/popularity";
 
 export default function Home() {
   return (
     <main className="container mx-auto">
       <Hero />
       <About />
+      <Topik />
+      <Popularity/>
     </main>
   );
 }
