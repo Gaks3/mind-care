ab77ed1b4fc063338eed05721d609b3ad812feee
arkhanardana <hanzmantap@gmail.com>
2025-03-12 19:01:56 +0700

    remove the unecessary import

--- src/components/articles.tsx
+++ src/components/articles.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import { ArrowRight, CalendarPlus } from "lucide-react";
-import Image from "next/image";
 import { Article } from "@/types";
+import ArticleCard from "./ui/article-card";
 
 const articles: Article[] = [
   {
@@ -54,54 +47,14 @@ const ArticleSection = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        {articles.map((article) => (
-          <Card
-            key={article.id}
-            className="overflow-hidden border-none rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 h-full flex flex-col"
-          >
-            <div className="relative h-48 overflow-hidden">
-              <Image
-                src={article.thumbnail}
-                alt={article.title}
-                width={400}
-                height={400}
-                className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-              />
-            </div>
-            <CardHeader className="pb-0 pt-5">
-              <p className="text-base text-gray-600 mt-2 flex items-center gap-2">
-                <CalendarPlus className="w-4 h-4 inline" />
-                {article.createdAt}
-              </p>
-              <h3 className="font-bold text-lg leading-tight text-gray-900">
-                {article.title}
-              </h3>
-            </CardHeader>
-            <CardContent className="py-2">
-              <div className="flex items-center text-gray-500 text-sm">
-                <Image
-                  src="/dokter1.webp"
-                  alt={article.createdBy}
-                  className="w-6 h-6 rounded-full mr-2 border border-gray-200"
-                  width={400}
-                  height={400}
-                />
-                <span className="font-medium">{article.createdBy}</span>
-              </div>
-            </CardContent>
-            <CardFooter className="mt-auto pt-2">
-              <a
-                href="#"
-                className="text-blue-600 text-sm font-semibold flex items-center group hover:text-blue-800 transition-colors"
-              >
-                Baca Selengkapnya
-                <ArrowRight
-                  size={16}
-                  className="ml-2 group-hover:translate-x-1 transition-transform"
-                />
-              </a>
-            </CardFooter>
-          </Card>
+        {articles.map((article, index) => (
+          <ArticleCard
+            id={article.id}
+            key={index}
+            title={article.title}
+            createdBy={article.createdBy}
+            thumbnail={article.thumbnail}
+            createdAt={article.createdAt} />
         ))}
       </div>
     </section>
