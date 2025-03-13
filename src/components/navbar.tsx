6a1d16c98b00d27dcb74c1f2c2dabf045adaaf68
arkhanardana <hanzmantap@gmail.com>
2025-03-13 19:53:50 +0700

    chore: edit from konseling to artikel Link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from "react"
 import { Button } from "@/components/ui/button"
-import { Menu, X, ChevronDown, Brain, CalendarHeart, BrainCircuit, HeartPulse, UserRoundSearch, Hospital, ScanHeart, Laugh, PersonStanding, HandHeart } from "lucide-react"
+import { Menu, X, ChevronDown, Brain, CalendarHeart, BrainCircuit, UserRoundSearch, Hospital, ScanHeart, Laugh, PersonStanding, HandHeart, Newspaper } from "lucide-react"
 import Link from "next/link"
 import { motion, AnimatePresence } from "framer-motion"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
@@ -37,7 +37,7 @@ export default function Navbar() {
 
           <nav className="hidden lg:flex items-center space-x-8">
             <Link href="#" className="text-gray-600 hover:text-primary px-3 py-2 text-sm font-medium flex items-center gap-2">
-              <HeartPulse className="ml-2 h-4 w-4" />Konseling
+              <Newspaper className="ml-2 h-4 w-4" />Artikel
             </Link>
 
             <DropdownMenu>
@@ -98,7 +98,7 @@ export default function Navbar() {
           <motion.div className="lg:hidden absolute w-full z-50" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3, ease: "easeInOut" }}>
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white shadow-lg">
               <Link href="#" className="block px-3 py-2 text-gray-600 hover:text-primary font-medium">Home</Link>
-              <Link href="#" className="block px-3 py-2 text-gray-600 hover:text-primary font-medium">Konseling</Link>
+              <Link href="#" className="block px-3 py-2 text-gray-600 hover:text-primary font-medium">Artikel</Link>
               <Link href="#" className="block px-3 py-2 text-gray-600 hover:text-primary font-medium">Tentang Kami</Link>
               <Link href="#" className="block px-3 py-2 text-gray-600 hover:text-primary font-medium">List Psikolog</Link>
 
