f8cbe275f1dfb41732c5928c4dd1bd24a33265c7
arkhanardana <hanzmantap@gmail.com>
2025-03-13 22:30:39 +0700

    chore: remove redirect

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,10 @@
 import { auth } from "@/lib/auth"
 import { headers } from "next/headers"
-import { redirect } from "next/navigation"
 
 export default async function Dashboard() {
   const session = await auth.api.getSession({
     headers: await headers(),
   })
-
-  if (!session) {
-    redirect("/")
-  }
 
   const user = session?.user
 
