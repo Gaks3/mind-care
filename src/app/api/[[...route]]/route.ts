91a39368dc593cc207858d99b74e5558e6b6c9b2
Gaks3 <dwi66116@gmail.com>
2025-03-14 20:44:02 +0700

    feat: handle error

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,8 +1,47 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { ZodError, ZodIssue } from "zod";
+import { routes } from "./routes";
+
 export const runtime = "nodejs";
 
 export const app = new Hono().basePath("/api");
+
+routes(app);
+
+app.onError((error, c) => {
+  if (error instanceof ZodError) {
+    const errors = error.errors.map((err) => {
+      if (err?.["unionErrors"]) {
+        return err["unionErrors"].flatMap((unionErr) =>
+          unionErr.issues.map((issue: ZodIssue) => ({
+            message: issue.message,
+            errorCode: issue.code,
+            path: issue.path,
+          }))
+        );
+      }
+
+      return {
+        message: err.message,
+        errorCode: err.code,
+        path: err.path,
+      };
+    });
+
+    return c.json({ error: errors.flat(), message: "ZodError" }, 400);
+  }
+
+  console.error(error);
+  return c.json(
+    { error, message: error.message || "Internal Server Error" },
+    500
+  );
+});
+
+app.notFound((c) => {
+  return c.text("404 Not Found", 404);
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
