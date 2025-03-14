ad578a84099f155b87edadc2ab26259c4e64e013
Gaks3 <dwi66116@gmail.com>
2025-03-14 20:40:21 +0700

    feat: add auto sign in and minimal password length configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,15 @@
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
-import db from './db';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import db from "./db";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
-    provider: 'postgresql',
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 8,
   },
-  appName: 'Mind Care',
+  appName: "Mind Care",
 });
