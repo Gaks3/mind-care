6a9fedde3afb17a733481dec31605e2c3b66f873
arkhanardana <hanzmantap@gmail.com>
2025-03-22 22:26:31 +0700

    chore: edit the title page to indonesia

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -28,7 +28,7 @@ export default async function PsychologistsList() {
 
   return (
     <div className="container mx-auto py-8 px-4 lg:px-24">
-      <h1 className="text-3xl font-bold mb-8">Our Psychologists</h1>
+      <h1 className="text-3xl font-bold mb-8">List Psikologi di MindCare</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {psychologists.map((psychologist) => (
