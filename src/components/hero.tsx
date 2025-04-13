224a9e300eea09f5e811b2e8588f5b309ec5ff4f
arkhanardana <hanzmantap@gmail.com>
2025-04-13 20:35:20 +0700

    style: changt the spacing bottom and top

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="pt-16 md:pt-24 lg:pt-44 px-4 sm:px-6 md:px-8 lg:px-12 overflow-hidden mb-14 lg:mb-64"
+      className="pt-20 px-4 sm:px-6 md:px-8 lg:px-12 overflow-hidden mb-20"
     >
       <div className="max-w-7xl mx-auto px-5 md:px-0 grid md:grid-cols-2 gap-8 md:gap-12 items-center">
         <div className="order-2 md:order-1 z-10">
