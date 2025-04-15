74883a0028e2f3582cbba161bdff7001d5ad6cfd
siddiqfathurahman <fr9299540@gmail.com>
2025-04-15 11:15:58 +0700

    style: edit some style for better user interface

--- src/components/ui/article-card.tsx
+++ src/components/ui/article-card.tsx
@@ -17,7 +17,7 @@ const ArticleCard = ({ datas }) => {
       {datas.map((e, i) => (
         <Card
           key={i}
-          className="overflow-hidden border-none rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 h-full flex flex-col"
+          className="w-full sm:w-[300px] md:w-[300px] lg:w-[300px] h-auto overflow-hidden border-none rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 flex flex-col"
         >
           <div className="relative h-48 overflow-hidden">
             <Image
@@ -29,7 +29,7 @@ const ArticleCard = ({ datas }) => {
             />
           </div>
           <CardHeader className="pb-0 pt-5">
-            <p className="text-base text-gray-600 mt-2 flex items-center gap-2">
+            <p className="text-xs text-gray-600 mt-2 flex items-center gap-2">
               <CalendarPlus className="w-4 h-4 inline" />
               <FormatDate dateString={e.createdAt} />
             </p>
@@ -39,19 +39,19 @@ const ArticleCard = ({ datas }) => {
           </CardHeader>
           <CardContent className="pt-1 pb-0">
             <div className="flex items-center text-gray-500 text-sm">
-              <Avatar className="h-10 w-10 flex items-center justify-center">
+              <Avatar className="flex items-center justify-center py-2">
                 <AvatarImage
                   src={e?.user.image || ""}
                   alt={e?.user.name || "User"}
                 />
                 <AvatarFallback className="text-black">
-                  <User className="w-6 h-6 text-gray-500" />
+                  <User className="w-6 h-6 mr-1 text-gray-500" />
                 </AvatarFallback>
               </Avatar>
-              <p className="sm:text-lg md:text-xl font-medium">{e.user.name}</p>
+              <p className="text-sm mt-2 font-medium">{e.user.name}</p>
             </div>
           </CardContent>
-          <CardFooter className="mt-auto pt-2">
+          <CardFooter className="mt- pt-2">
             <Link
               href={`/articles/${e.id}`}
               className="text-blue-600 text-sm font-semibold flex items-center group hover:text-blue-800 transition-colors"
